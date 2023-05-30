d69cb159ca83ec3d58927e1e44dd57094a44e0c1
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-05-30 17:29:38 -0300

    cria rota de admin

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,13 +1,35 @@
-import styles from './styles.module.css';
-import Head from 'next/head';
+import { GetServerSideProps } from "next";
+import styles from "./styles.module.css";
+import Head from "next/head";
 
-export default function Dashboard(){
-    return(
-        <div className={styles.container}>
-            <Head>
-                <title>Meu painel de tarefas</title>
-            </Head>
-            <h1>Página Painel</h1>
-        </div>
-    )
+import { getSession } from "next-auth/react";
+
+export default function Dashboard() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Meu painel de tarefas</title>
+      </Head>
+      <h1>Página Painel</h1>
+    </div>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  // console.log("BUSCANDO PELO SSR")
+  const session = await getSession({ req });
+  //   console.log(session);
+  if (!session?.user) {
+    // se nao tem usuario vamos direcionar para a home /
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
