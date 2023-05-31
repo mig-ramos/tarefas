9b89e0e16750b37237264890b797576966dc8553
Miguel Arcanjo <mig.ramos10@gmail.com>
2023-05-31 16:38:30 -0300

    realiza comentarios

--- src/pages/task/[id].tsx
+++ src/pages/task/[id].tsx
@@ -1,22 +1,56 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { useSession } from "next-auth/react";
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import styles from "./styles.module.css";
 
 import { db } from "@/services/firebaseConnection";
-import { doc, collection, query, where, getDoc } from "firebase/firestore";
+import {
+  doc,
+  collection,
+  query,
+  where,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
 import { Textarea } from "../../components/textarea";
 
 interface TaskProps {
-    item: {
-      tarefa: string;
-      created: string;
-      public: boolean;
-      user: string;
-      taskId: string;
-    };
-  }
+  item: {
+    tarefa: string;
+    created: string;
+    public: boolean;
+    user: string;
+    taskId: string;
+  };
+}
 
 export default function Task({ item }: TaskProps) {
+  const { data: session } = useSession();
+
+  const [input, setInput] = useState("");
+
+  async function handleComment(event: FormEvent) {
+    event.preventDefault();
+
+    if (input === "") return;
+
+    if (!session?.user?.email || !session?.user?.name) return;
+
+    try {
+      const docRef = await addDoc(collection(db, "comments"), {
+        comment: input,
+        created: new Date(),
+        user: session?.user?.email,
+        name: session?.user?.name,
+        taskId: item?.taskId,
+      });
+
+      setInput("");
+    } catch (err) {
+      console.log(err);
+    }
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -33,9 +67,17 @@ export default function Task({ item }: TaskProps) {
       <section className={styles.commentsContainer}>
         <h2>Deixar comentário</h2>
 
-        <form>
-          <Textarea placeholder="Digite seu comentário..." />
-          <button className={styles.button}>Enviar comentário</button>
+        <form onSubmit={handleComment}>
+          <Textarea
+            value={input}
+            onChange={(event: ChangeEvent<HTMLTextAreaElement>) =>
+              setInput(event.target.value)
+            }
+            placeholder="Digite seu comentário..."
+          />
+          <button disabled={!session?.user} className={styles.button}>
+            Enviar comentário
+          </button>
         </form>
       </section>
     </div>
@@ -77,7 +119,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     taskId: id,
   };
 
-  console.log(task);
+  //   console.log(task);
 
   return {
     props: {
